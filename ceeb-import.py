467067ad81113193602611a720b487462d4315f1
Samuel Damashek <samuel.damashek@gmail.com>
2016-12-13 20:54:03 -0500

    Allow for duplicates in ceeb import

--- ceeb-import.py
+++ ceeb-import.py
@@ -2,4 +2,6 @@ import csv
 reader = csv.reader(open("ceeb.csv", "r"))
 for row in reader:
     ceeb, name, city, state = row
-    College.objects.create(ceeb=ceeb.lstrip('0'), name=("{} - {}, {}".format(name, city, state)).title())
+    ceeb = ceeb.lstrip('0')
+    if College.objects.filter(ceeb=ceeb).count() == 0:
+        College.objects.create(ceeb=ceeb, name=("{} - {}, {}".format(name, city, state)).title())
