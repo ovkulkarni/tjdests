90b3ef9739c3867cc6f28383e0bcee8bbcea57ef
Samuel Damashek <samuel.damashek@gmail.com>
2016-12-13 20:56:39 -0500

    International format compatibility

--- ceeb-import.py
+++ ceeb-import.py
@@ -1,7 +1,14 @@
 import csv
 reader = csv.reader(open("ceeb.csv", "r"))
+numeric = '0123456789'
 for row in reader:
-    ceeb, name, city, state = row
+    ceeb, name = row[:2]
     ceeb = ceeb.lstrip('0')
+    if ceeb[0] not in numeric:
+        state = ceeb[:2]
+        city = 'INTL'
+    else:
+        state, city = row[2:4]
+
     if College.objects.filter(ceeb=ceeb).count() == 0:
         College.objects.create(ceeb=ceeb, name=("{} - {}, {}".format(name, city, state)).title())
