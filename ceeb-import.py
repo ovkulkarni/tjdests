e31605c3edc6061926b84dcbcbd5d7b35cb30dd7
Samuel Damashek <samuel.damashek@gmail.com>
2016-12-13 21:00:02 -0500

    Better international compat

--- ceeb-import.py
+++ ceeb-import.py
@@ -4,11 +4,11 @@ numeric = '0123456789'
 for row in reader:
     ceeb, name = row[:2]
     ceeb = ceeb.lstrip('0')
-    if ceeb[0] not in numeric:
-        state = ceeb[:2]
-        city = 'INTL'
-    else:
-        state, city = row[2:4]
-
     if College.objects.filter(ceeb=ceeb).count() == 0:
+        if ceeb[0] not in numeric:
+            state = ceeb[:2]
+            city = 'INTL'
+        else:
+            state, city = row[2:4]
+
         College.objects.create(ceeb=ceeb, name=("{} - {}, {}".format(name, city, state)).title())
