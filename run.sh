7d1a1cd3c722a0e5029b8fc11ae327af8d28d1e9
Naitian Zhou <nzhou2011@gmail.com>
2017-10-26 14:47:19 -0400

    activites -> projects

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /web/activities/tjdests2018/tjdests/
-/web/activities/tjdests2018/tjdests/env/bin/gunicorn tjdests.wsgi -b 127.0.0.1:$PORT -w=15
+cd /web/projects/tjdests2018/tjdests/
+/web/projects/tjdests2018/tjdests/env/bin/gunicorn tjdests.wsgi -b 127.0.0.1:$PORT -w=15
 
